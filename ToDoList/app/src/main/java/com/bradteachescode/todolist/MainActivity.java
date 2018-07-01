d0516b7fa9a357c97a1f20be48aa86eb5a67b262
Brad <bradteachescode@gmail.com>
2018-07-01 08:24:24 -0400

    added delete from file
    file delete added

--- ToDoList/app/src/main/java/com/bradteachescode/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/bradteachescode/todolist/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         items.remove(position);
         adapter.notifyDataSetChanged();
+        FileHelper.writeData(items, this);
         Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
 
     }
